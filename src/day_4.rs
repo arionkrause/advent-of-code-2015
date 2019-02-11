775e9dcb9abd716266c9caa7b2e063d5bd30ae5d
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-11 16:11:54 -0200

    Day 4 refactoring

--- src/day_4.rs
+++ src/day_4.rs
@@ -1,27 +1,30 @@
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+
 pub fn solve(input: &str) -> (Option<i32>, Option<i32>) {
     print!("Day {}", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     let mut part_1 = None;
     let mut part_2 = None;
-
+    let mut md5_hasher = Md5::new();
     let mut number = 1;
     let expected_starting_part_1 = "0".repeat(5);
     let expected_starting_part_2 = "0".repeat(6);
 
     loop {
-        let mut attempt: String = input.clone().to_string();
-        attempt.push_str(&number.to_string());
-        let digest = md5::compute(attempt);
-        let digest_in_hexadecimal = format!("{:x}", digest);
+        let mut attempt = format!("{}{}", input, number);
+        md5_hasher.input_str(&attempt);
+        let digest = md5_hasher.result_str();
 
-        if part_1.is_none() && digest_in_hexadecimal.starts_with(&expected_starting_part_1) {
+        if part_1.is_none() && digest.starts_with(&expected_starting_part_1) {
             part_1 = Some(number);
         }
 
-        if digest_in_hexadecimal.starts_with(&expected_starting_part_2) {
+        if digest.starts_with(&expected_starting_part_2) {
             part_2 = Some(number);
             break;
         }
 
+        md5_hasher.reset();
         number += 1;
     }
 
