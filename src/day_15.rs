e6450e39a22f748b1999f7863244e24e689e0570
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-18 14:29:39 -0300

    Day 15

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,17 +1,17 @@
 use regex::Regex;
 
-#[derive(Debug)]
 struct Ingredient {
     capacity: i8,
     durability: i8,
     flavor: i8,
     texture: i8,
-    calories: i8,
+    calories: u8,
 }
 
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -34,9 +34,32 @@ fn decode_input(input: &str) -> Vec<Ingredient> {
     ingredients
 }
 
+fn get_score(ingredients: &Vec<Ingredient>, amounts: &Vec<u8>) -> u32 {
+    let mut total_capacity = 0;
+    let mut total_durability = 0;
+    let mut total_flavor = 0;
+    let mut total_texture = 0;
+
+    for (index, ingredient) in ingredients.iter().enumerate() {
+        total_capacity += amounts[index] as i32 * ingredient.capacity as i32;
+        total_durability += amounts[index] as i32 * ingredient.durability as i32;
+        total_flavor += amounts[index] as i32 * ingredient.flavor as i32;
+        total_texture += amounts[index] as i32 * ingredient.texture as i32;
+    }
+
+    if total_capacity < 0
+            || total_durability < 0
+            || total_flavor < 0
+            || total_texture < 0 {
+        return 0;
+    }
+
+    total_capacity as u32 * total_durability as u32 * total_flavor as u32 * total_texture as u32
+}
+
 mod part_1 {
     use crate::day_15::decode_input;
-    use crate::day_15::Ingredient;
+    use crate::day_15::get_score;
 
     pub fn solve(input: &str) -> u32 {
         let ingredients = decode_input(&input);
@@ -79,33 +102,72 @@ mod part_1 {
         }
     }
 
-    fn get_score(ingredients: &Vec<Ingredient>, amounts: &Vec<u8>) -> u32 {
-        let mut total_capacity = 0;
-        let mut total_durability = 0;
-        let mut total_flavor = 0;
-        let mut total_texture = 0;
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        assert_eq!(solve("Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"), 62842880);
+    }
+}
 
-        for (index, ingredient) in ingredients.iter().enumerate() {
-            total_capacity += amounts[index] as i32 * ingredient.capacity as i32;
-            total_durability += amounts[index] as i32 * ingredient.durability as i32;
-            total_flavor += amounts[index] as i32 * ingredient.flavor as i32;
-            total_texture += amounts[index] as i32 * ingredient.texture as i32;
+mod part_2 {
+    use crate::day_15::Ingredient;
+    use crate::day_15::decode_input;
+    use crate::day_15::get_score;
+
+    pub fn solve(input: &str) -> u32 {
+        let ingredients = decode_input(&input);
+        let mut best_score = None;
+        let mut amounts = vec![0; ingredients.len()];
+
+        loop {
+            let last_index = amounts.len() - 1;
+            amounts[last_index] += 1;
+
+            if amounts[amounts.len() - 1] > 100 {
+                let mut index = amounts.len() - 1;
+                amounts[index] = 0;
+                index -= 1;
+
+                loop {
+                    amounts[index] += 1;
+
+                    if amounts[index] > 100 {
+                        if index == 0 {
+                            return best_score.unwrap();
+                        }
+
+                        amounts[index] = 0;
+                        index -= 1;
+                        continue;
+                    }
+
+                    break;
+                }
+            }
+
+            if amounts.iter().map(|a| *a as u16 ).sum::<u16>() == 100 {
+                let score = get_score(&ingredients, &amounts);
+
+                if (best_score.is_none() || score > best_score.unwrap())
+                        && get_calories_amount(&ingredients, &amounts) == 500 {
+                    best_score = Some(score);
+                }
+            }
         }
+    }
 
-        if total_capacity < 0
-                || total_durability < 0
-                || total_flavor < 0
-                || total_texture < 0 {
-            return 0;
-        }
-
-        total_capacity as u32 * total_durability as u32 * total_flavor as u32 * total_texture as u32
+    fn get_calories_amount(ingredients: &Vec<Ingredient>, amounts: &Vec<u8>) -> u16 {
+        ingredients.iter()
+                .enumerate()
+                .map(|(index, ingredient)| amounts[index] as u16 * ingredient.calories as u16)
+                .sum()
     }
 
     #[cfg(test)]
     #[test]
     fn test_1() {
         assert_eq!(solve("Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
-Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"), 62842880);
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"), 57600000);
     }
 }
