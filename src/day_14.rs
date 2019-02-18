21338be3ac87ad842fc83a2823a1fae5b8aa7f93
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-18 09:16:06 -0300

    Day 14 alternative

--- src/day_14.rs
+++ src/day_14.rs
@@ -12,34 +12,13 @@ mod part_1 {
         distance_travelled_per_second_in_kilometers: usize,
         stamina_in_seconds: usize,
         rest_period_in_seconds: usize,
-        distance_travelled_so_far_in_kilometers: usize,
-        stamina_left_in_seconds: usize,
-        rest_period_left_in_seconds: usize,
     }
 
     pub fn solve(input: &str, race_duration_in_seconds: usize) -> usize {
-        let mut reindeers = decode_input(&input);
-
-        for _ in 0..race_duration_in_seconds {
-            reindeers.iter_mut().for_each(|reindeer| {
-                if reindeer.rest_period_left_in_seconds == 0 {
-                    reindeer.distance_travelled_so_far_in_kilometers += reindeer.distance_travelled_per_second_in_kilometers;
-                    reindeer.stamina_left_in_seconds -= 1;
-
-                    if reindeer.stamina_left_in_seconds == 0 {
-                        reindeer.rest_period_left_in_seconds = reindeer.rest_period_in_seconds;
-                    }
-                } else {
-                    reindeer.rest_period_left_in_seconds -= 1;
-
-                    if reindeer.rest_period_left_in_seconds == 0 {
-                        reindeer.stamina_left_in_seconds = reindeer.stamina_in_seconds;
-                    }
-                }
-            });
-        }
-
-        reindeers.iter().map(|r| r.distance_travelled_so_far_in_kilometers).max().unwrap()
+        decode_input(&input).iter().map(|reindeer| {
+            reindeer.distance_travelled_per_second_in_kilometers * reindeer.stamina_in_seconds * (race_duration_in_seconds / (reindeer.stamina_in_seconds + reindeer.rest_period_in_seconds))
+                    + reindeer.distance_travelled_per_second_in_kilometers * reindeer.stamina_in_seconds.min(race_duration_in_seconds % (reindeer.stamina_in_seconds + reindeer.rest_period_in_seconds))
+        }).max().unwrap()
     }
 
     fn decode_input(input: &str) -> Vec<Reindeer> {
@@ -48,15 +27,11 @@ mod part_1 {
 
         for line in input.lines() {
             let captures = re.captures(&line).unwrap();
-            let stamina_in_seconds = captures.name("stamina_in_seconds").unwrap().as_str().parse().unwrap();
 
             reindeers.push(Reindeer {
                 distance_travelled_per_second_in_kilometers: captures.name("distance_travelled_per_second_in_kilometers").unwrap().as_str().parse().unwrap(),
-                stamina_in_seconds,
+                stamina_in_seconds: captures.name("stamina_in_seconds").unwrap().as_str().parse().unwrap(),
                 rest_period_in_seconds: captures.name("rest_period_in_seconds").unwrap().as_str().parse().unwrap(),
-                distance_travelled_so_far_in_kilometers: 0,
-                stamina_left_in_seconds: stamina_in_seconds,
-                rest_period_left_in_seconds: 0,
             });
         }
 
