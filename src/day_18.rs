5244e1db2120290352fc1094359eea7d42f2d2ca
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-19 10:30:43 -0300

    Day 18 refactor

--- src/day_18.rs
+++ src/day_18.rs
@@ -5,68 +5,70 @@ pub fn solve(input: &str) {
     println!();
 }
 
+fn decode_input(input: &str) -> Vec<Vec<char>> {
+    let mut grid = Vec::new();
+    grid.push(vec!['.'; input.lines().next().unwrap().len() + 2]);
+
+    for line in input.lines() {
+        let mut row = Vec::with_capacity(line.len());
+        row.push('.');
+
+        for tile in line.chars() {
+            row.push(tile);
+        }
+
+        row.push('.');
+        grid.push(row);
+    }
+
+    grid.push(vec!['.'; input.lines().next().unwrap().len() + 2]);
+    grid
+}
+
+fn get_amount_neighbors_on(grid: &Vec<Vec<char>>, y: usize, x: usize) -> u8 {
+    let mut amount_neighbors_on = 0;
+
+    if grid[y - 1][x - 1] == '#' { amount_neighbors_on += 1; }
+    if grid[y - 1][x] == '#' { amount_neighbors_on += 1; }
+    if grid[y - 1][x + 1] == '#' { amount_neighbors_on += 1; }
+
+    if grid[y][x - 1] == '#' { amount_neighbors_on += 1; }
+    if grid[y][x + 1] == '#' { amount_neighbors_on += 1; }
+
+    if grid[y + 1][x - 1] == '#' { amount_neighbors_on += 1; }
+    if grid[y + 1][x] == '#' { amount_neighbors_on += 1; }
+    if grid[y + 1][x + 1] == '#' { amount_neighbors_on += 1; }
+
+    amount_neighbors_on
+}
+
 mod part_1 {
+    use crate::day_18::decode_input;
+    use crate::day_18::get_amount_neighbors_on;
+
     pub fn solve(input: &str, steps: u8) -> u16 {
         let mut grid = decode_input(&input);
+        let mut buffer_grid = grid.clone();
 
         for _ in 0..steps {
-            let mut new_grid = Vec::with_capacity(grid.len());
-
-            for (y, row) in grid.iter().enumerate() {
-                let mut new_row = Vec::with_capacity(row.len());
-
-                for (x, tile) in row.iter().enumerate() {
+            for (y, row) in grid.iter().enumerate().skip(1).take(grid.len() - 2) {
+                for (x, tile) in row.iter().enumerate().skip(1).take(row.len() - 2) {
                     let amount_neighbors_on = get_amount_neighbors_on(&grid, y, x);
 
                     if *tile == '#' && amount_neighbors_on != 2 && amount_neighbors_on != 3 {
-                        new_row.push('.');
+                        buffer_grid[y][x] = '.';
                     } else if *tile == '.' && amount_neighbors_on == 3 {
-                        new_row.push('#');
+                        buffer_grid[y][x] = '#';
                     } else {
-                        new_row.push(*tile);
+                        buffer_grid[y][x] = *tile;
                     }
                 }
-
-                new_grid.push(new_row);
             }
 
-            grid = new_grid;
+            std::mem::swap(&mut grid, &mut buffer_grid);
         }
 
         grid.iter().map(|row| row.iter().filter(|&tile| *tile == '#').count() as u16).sum()
-    }
-
-    fn decode_input(input: &str) -> Vec<Vec<char>> {
-        let mut grid = Vec::new();
-
-        for line in input.lines() {
-            let mut row = Vec::with_capacity(line.len());
-
-            for tile in line.chars() {
-                row.push(tile);
-            }
-
-            grid.push(row);
-        }
-
-        grid
-    }
-
-    fn get_amount_neighbors_on(grid: &Vec<Vec<char>>, y: usize, x: usize) -> u8 {
-        let mut amount_neighbors_on = 0;
-
-        if y > 0 && x > 0 && grid[y - 1][x - 1] == '#' { amount_neighbors_on += 1; }
-        if y > 0 && grid[y - 1][x] == '#' { amount_neighbors_on += 1; }
-        if y > 0 && x < grid[y].len() - 1 && grid[y - 1][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        if x > 0 && grid[y][x - 1] == '#' { amount_neighbors_on += 1; }
-        if x < grid[y].len() - 1 && grid[y][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        if y < grid.len() - 1 && x > 0 && grid[y + 1][x - 1] == '#' { amount_neighbors_on += 1; }
-        if y < grid.len() - 1 && grid[y + 1][x] == '#' { amount_neighbors_on += 1; }
-        if y < grid.len() - 1 && x < grid[0].len() - 1 && grid[y + 1][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        amount_neighbors_on
     }
 
     #[cfg(test)]
@@ -84,90 +86,42 @@ mod part_1 {
 }
 
 mod part_2 {
+    use crate::day_18::decode_input;
+    use crate::day_18::get_amount_neighbors_on;
+
     pub fn solve(input: &str, steps: u8) -> u16 {
         let mut grid = decode_input(&input);
         let last_grid_y_index = grid.len() - 1;
         let last_grid_x_index = grid[0].len() - 1;
-        grid[0][0] = '#';
-        grid[0][last_grid_x_index] = '#';
-        grid[last_grid_y_index][0] = '#';
-        grid[last_grid_y_index][last_grid_x_index] = '#';
+        grid[1][1] = '#';
+        grid[1][last_grid_x_index - 1] = '#';
+        grid[last_grid_y_index - 1][1] = '#';
+        grid[last_grid_y_index - 1][last_grid_x_index - 1] = '#';
+        let mut buffer_grid = grid.clone();
 
         for _ in 0..steps {
-            let mut new_grid = Vec::with_capacity(grid.len());
-
-            for (y, row) in grid.iter().enumerate() {
-                let mut new_row = Vec::with_capacity(row.len());
-
-                for (x, tile) in row.iter().enumerate() {
-                    if (y == 0 || y == grid.len() - 1) && (x == 0 || x == row.len() - 1) {
-                        new_row.push(*tile);
+            for (y, row) in grid.iter().enumerate().skip(1).take(grid.len() - 2) {
+                for (x, tile) in row.iter().enumerate().skip(1).take(row.len() - 2) {
+                    if (y == 1 || y == grid.len() - 2) && (x == 1 || x == row.len() - 2) {
+                        buffer_grid[y][x] = *tile;
                         continue;
                     }
 
                     let amount_neighbors_on = get_amount_neighbors_on(&grid, y, x);
 
                     if *tile == '#' && amount_neighbors_on != 2 && amount_neighbors_on != 3 {
-                        new_row.push('.');
+                        buffer_grid[y][x] = '.';
                     } else if *tile == '.' && amount_neighbors_on == 3 {
-                        new_row.push('#');
+                        buffer_grid[y][x] = '#';
                     } else {
-                        new_row.push(*tile);
+                        buffer_grid[y][x] = *tile;
                     }
                 }
-
-                new_grid.push(new_row);
             }
 
-            grid = new_grid;
+            std::mem::swap(&mut grid, &mut buffer_grid);
         }
 
         grid.iter().map(|row| row.iter().filter(|&tile| *tile == '#').count() as u16).sum()
-    }
-
-    fn decode_input(input: &str) -> Vec<Vec<char>> {
-        let mut grid = Vec::new();
-
-        for line in input.lines() {
-            let mut row = Vec::with_capacity(line.len());
-
-            for tile in line.chars() {
-                row.push(tile);
-            }
-
-            grid.push(row);
-        }
-
-        grid
-    }
-
-    fn get_amount_neighbors_on(grid: &Vec<Vec<char>>, y: usize, x: usize) -> u8 {
-        let mut amount_neighbors_on = 0;
-
-        if y > 0 && x > 0 && grid[y - 1][x - 1] == '#' { amount_neighbors_on += 1; }
-        if y > 0 && grid[y - 1][x] == '#' { amount_neighbors_on += 1; }
-        if y > 0 && x < grid[y].len() - 1 && grid[y - 1][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        if x > 0 && grid[y][x - 1] == '#' { amount_neighbors_on += 1; }
-        if x < grid[y].len() - 1 && grid[y][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        if y < grid.len() - 1 && x > 0 && grid[y + 1][x - 1] == '#' { amount_neighbors_on += 1; }
-        if y < grid.len() - 1 && grid[y + 1][x] == '#' { amount_neighbors_on += 1; }
-        if y < grid.len() - 1 && x < grid[0].len() - 1 && grid[y + 1][x + 1] == '#' { amount_neighbors_on += 1; }
-
-        amount_neighbors_on
-    }
-
-    #[cfg(test)]
-    #[test]
-    fn test_1() {
-        let input = ".#.#.#
-...##.
-#....#
-..#...
-#.#..#
-####..";
-
-        assert_eq!(solve(&input, 5), 17);
     }
 }
