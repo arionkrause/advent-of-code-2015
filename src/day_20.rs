42471f1bd845fba335919d01a0a734f8a9e5f16c
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-20 08:55:51 -0300

    Day 20 Part 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -1,17 +1,17 @@
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
-//    println!("Part 2: {}.", part_2::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
 mod part_1 {
     pub fn solve(input: &str) -> u32 {
         let input_as_number = input.parse::<u32>().unwrap();
-        let mut houses_presents = vec![0; input_as_number as usize + 1];
+        let mut houses_presents = vec![0; (input_as_number / 10) as usize + 1];
 
-        for elf_number in 1..=input_as_number as usize {
-            for house_number in (elf_number..=input_as_number as usize).step_by(elf_number) {
+        for elf_number in 1..houses_presents.len() as usize {
+            for house_number in (elf_number..houses_presents.len()).step_by(elf_number) {
                 houses_presents[house_number] += elf_number as u32 * 10;
             }
         }
@@ -24,31 +24,6 @@ mod part_1 {
 
         unreachable!()
     }
-//    pub fn solve(input: &str) -> u32 {
-//        let input_as_number = input.parse::<u32>().unwrap();
-//        let mut houses_presents = Vec::with_capacity(input_as_number as usize);
-//
-//        for i in 1..=houses_presents.len() {
-//            let mut count = 0;
-//
-//            for j in i..=houses_presents.len() {
-//                houses_presents[j] += i as u32 * 11;
-//                count += 1;
-//
-//                if count == 50 {
-//                    break;
-//                }
-//            }
-//        }
-//
-//        for i in 0..houses_presents.len() {
-//            if houses_presents[i] >= input_as_number {
-//                return houses_presents[i];
-//            }
-//        }
-//
-//        unreachable!()
-//    }
 
     #[cfg(test)]
     #[test]
@@ -94,5 +69,33 @@ mod part_1 {
     #[test]
     fn test_9() {
         assert_eq!(solve("130"), 8);
+    }
+}
+
+mod part_2 {
+    pub fn solve(input: &str) -> u32 {
+        let input_as_number = input.parse::<u32>().unwrap();
+        let mut houses_presents = vec![0; (input_as_number / 10) as usize + 1];
+
+        for elf_number in 1..houses_presents.len() {
+            let mut count = 0;
+
+            for house_number in (elf_number..houses_presents.len()).step_by(elf_number) {
+                houses_presents[house_number] += elf_number as u32 * 11;
+                count += 1;
+
+                if count == 50 {
+                    break;
+                }
+            }
+        }
+
+        for house_number in 0..houses_presents.len() {
+            if houses_presents[house_number] >= input_as_number {
+                return house_number as u32;
+            }
+        }
+
+        unreachable!()
     }
 }
