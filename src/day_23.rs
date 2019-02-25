e01df106a801195d15f72617237e89d6b298868b
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-25 15:56:11 -0300

    Day 23 Part 2

--- src/day_23.rs
+++ src/day_23.rs
@@ -13,6 +13,7 @@ enum Instruction {
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -61,71 +62,81 @@ fn decode_input(input: &str) -> Vec<Instruction> {
     instructions
 }
 
-mod part_1 {
-    use crate::day_23::decode_input;
-    use crate::day_23::Instruction;
+fn compute(input: &str, mut register_a: u32) -> u32 {
+    let instructions = decode_input(&input);
+    let mut register_b = 0u32;
+    let mut program_counter = 0;
 
-    pub fn solve(input: &str) -> u32 {
-        let instructions = decode_input(&input);
-        let mut register_a = 0u32;
-        let mut register_b = 0u32;
-        let mut program_counter = 0;
-
-        while program_counter < instructions.len() {
-            match instructions[program_counter] {
-                Instruction::Hlf { register } => {
-                    if register == 'a' {
-                        register_a /= 2;
-                    } else {
-                        register_b /= 2;
-                    }
-
-                    program_counter += 1;
+    while program_counter < instructions.len() {
+        match instructions[program_counter] {
+            Instruction::Hlf { register } => {
+                if register == 'a' {
+                    register_a /= 2;
+                } else {
+                    register_b /= 2;
                 }
-                Instruction::Inc { register } => {
-                    if register == 'a' {
-                        register_a += 1;
-                    } else {
-                        register_b += 1;
-                    }
 
-                    program_counter += 1;
+                program_counter += 1;
+            }
+            Instruction::Inc { register } => {
+                if register == 'a' {
+                    register_a += 1;
+                } else {
+                    register_b += 1;
                 }
-                Instruction::Jie { register, offset } => {
-                    if (register == 'a' && register_a % 2 == 0)
-                            || register == 'b' && register_b % 2 == 0 {
-                        program_counter += offset as usize;
-                    } else {
-                        program_counter += 1;
-                    }
-                }
-                Instruction::Jio { register, offset } => {
-                    if (register == 'a' && register_a == 1)
-                            || register == 'b' && register_b == 1 {
-                        program_counter += offset as usize;
-                    } else {
-                        program_counter += 1;
-                    }
-                }
-                Instruction::Jmp { offset } => {
-                    if (program_counter as isize + offset as isize) < 0 {
-                        break;
-                    }
 
-                    program_counter = (program_counter as isize + offset as isize) as usize;
-                }
-                Instruction::Tpl { register } => {
-                    if register == 'a' {
-                        register_a *= 3;
-                    } else {
-                        register_b *= 3;
-                    }
-
+                program_counter += 1;
+            }
+            Instruction::Jie { register, offset } => {
+                if (register == 'a' && register_a % 2 == 0)
+                        || register == 'b' && register_b % 2 == 0 {
+                    program_counter += offset as usize;
+                } else {
                     program_counter += 1;
                 }
             }
-        }
+            Instruction::Jio { register, offset } => {
+                if (register == 'a' && register_a == 1)
+                        || register == 'b' && register_b == 1 {
+                    program_counter += offset as usize;
+                } else {
+                    program_counter += 1;
+                }
+            }
+            Instruction::Jmp { offset } => {
+                if (program_counter as isize + offset as isize) < 0 {
+                    break;
+                }
 
-        register_b
+                program_counter = (program_counter as isize + offset as isize) as usize;
+            }
+            Instruction::Tpl { register } => {
+                if register == 'a' {
+                    register_a *= 3;
+                } else {
+                    register_b *= 3;
+                }
+
+                program_counter += 1;
+            }
+        }
+    }
+
+    register_b
+}
+
+mod part_1 {
+    use crate::day_23::compute;
+
+    pub fn solve(input: &str) -> u32 {
+        compute(&input, 0)
+    }
+}
+
+mod part_2 {
+    use crate::day_23::compute;
+
+    pub fn solve(input: &str) -> u32 {
+        compute(&input, 1)
     }
 }
